a1b9a174ffbf602c4f9aa23657f23f4277ccb2b6
tuantmtb <tuantmtb@gmail.com>
2019-07-14 22:17:02 +0700

    crawl diemthi dai hoc 2019

--- crawler/diemthi2019/crawler_diemthi2019.py
+++ crawler/diemthi2019/crawler_diemthi2019.py
@@ -17,55 +17,58 @@ def get_url_check_sbd(sbd):
 
 
 def get_info(sbd):
-    url_diemthi = get_url_check_sbd(sbd=str(sbd))
-    response = requests.get(url_diemthi)
     info_obj = None
-    if response.status_code == 200:
-        soup = BeautifulSoup(response.text, 'html.parser')
-        # logger.info(soup)
-        if soup.select_one('#ctl01_SoBD') is not None:
-            _sbd = soup.select_one('#ctl01_SoBD').get_text().strip()
-            points = []
-            info_obj = info_obj = {
-                'sbd': _sbd,
-            }
-            for idx, td in enumerate(soup.select('.color-red')):
-                if td.get_text().strip() != '-':
-                    point = None
-                    subject = None
-                    if idx == 9:
-                        point = td.get_text().strip()
-                    else:
-                        point = float(td.get_text().strip())
-                    # logger.info(point)
+    try:
+        url_diemthi = get_url_check_sbd(sbd=str(sbd))
+        response = requests.get(url_diemthi)
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.text, 'html.parser')
+            # logger.info(soup)
+            if soup.select_one('#ctl01_SoBD') is not None:
+                _sbd = soup.select_one('#ctl01_SoBD').get_text().strip()
+                points = []
+                info_obj = info_obj = {
+                    'sbd': _sbd,
+                }
+                for idx, td in enumerate(soup.select('.color-red')):
+                    if td.get_text().strip() != '-':
+                        point = None
+                        subject = None
+                        if idx == 9:
+                            point = td.get_text().strip()
+                        else:
+                            point = float(td.get_text().strip())
+                        # logger.info(point)
 
-                    if idx == 0:
-                        subject = 'Toan'
-                    elif idx == 1:
-                        subject = 'Van'
-                    elif idx == 2:
-                        subject = 'Li'
-                    elif idx == 3:
-                        subject = 'Hoa'
-                    elif idx == 4:
-                        subject = 'Sinh'
-                    elif idx == 5:
-                        subject = 'Su'
-                    elif idx == 6:
-                        subject = 'Đia'
-                    elif idx == 7:
-                        subject = 'GDCD'
-                    elif idx == 8:
-                        subject = 'Ngoai_ngu'
-                    elif idx == 9:
-                        subject = 'Ma_mon_ngoai_ngu'
+                        if idx == 0:
+                            subject = 'Toan'
+                        elif idx == 1:
+                            subject = 'Van'
+                        elif idx == 2:
+                            subject = 'Li'
+                        elif idx == 3:
+                            subject = 'Hoa'
+                        elif idx == 4:
+                            subject = 'Sinh'
+                        elif idx == 5:
+                            subject = 'Su'
+                        elif idx == 6:
+                            subject = 'Đia'
+                        elif idx == 7:
+                            subject = 'GDCD'
+                        elif idx == 8:
+                            subject = 'Ngoai_ngu'
+                        elif idx == 9:
+                            subject = 'Ma_mon_ngoai_ngu'
 
-                    info_obj.update({
-                        subject: point
-                    })
+                        info_obj.update({
+                            subject: point
+                        })
 
-            # logger.info(f'found {sbd}')
-            logger.info(info_obj)
+                # logger.info(f'found {sbd}')
+                logger.info(info_obj)
+    except Exception as e:
+        logger.error(e)
     return info_obj
 
 
@@ -112,22 +115,25 @@ if __name__ == '__main__':
 
     lst_provide = ['{0:02}'.format(num) for num in range(1, 65)]
     for provide_id in lst_provide:
-        logger.info(f'prepare crawl provide: {provide_id}')
-        # provide_id = 64
-        batch_sbd = 5000
+        try:
+            logger.info(f'prepare crawl provide: {provide_id}')
+            # provide_id = 64
+            batch_sbd = 5000
 
-        max_sbd = get_min_max_by_code(provide_id)
-        # logger.info(max_sbd)
-        # max_sbd = 5743
-        lst_sbd = []
-        for pos in range(1, max_sbd):
-            sbd = build_sbd(provide_id=provide_id, post_sbd=pos)
-            lst_sbd.append(sbd)
+            max_sbd = get_min_max_by_code(provide_id)
+            # logger.info(max_sbd)
+            # max_sbd = 5743
+            lst_sbd = []
+            for pos in range(1, max_sbd):
+                sbd = build_sbd(provide_id=provide_id, post_sbd=pos)
+                lst_sbd.append(sbd)
 
-        for idx, sub_lst_sbd in enumerate(get_sublists(lst_sbd, int(len(lst_sbd) / 5000) + 1)):
-            file_diemthi_path = ConfigUniversityProject().file_diemthi_2019_path(provide_id=provide_id, part=idx)
-            obj_sbd = multithread_helper(items=sub_lst_sbd, method=get_info, timeout_concurrent_by_second=36000,
-                                         max_workers=50, debug=False)
-            store_jsons_perline_in_file(jsons_obj=obj_sbd, file_output_path=file_diemthi_path)
-            logger.info(f'write: {file_diemthi_path}')
+            for idx, sub_lst_sbd in enumerate(get_sublists(lst_sbd, int(len(lst_sbd) / 5000) + 1)):
+                file_diemthi_path = ConfigUniversityProject().file_diemthi_2019_path(provide_id=provide_id, part=idx)
+                obj_sbd = multithread_helper(items=sub_lst_sbd, method=get_info, timeout_concurrent_by_second=36000,
+                                             max_workers=50, debug=False)
+                store_jsons_perline_in_file(jsons_obj=obj_sbd, file_output_path=file_diemthi_path)
+                logger.info(f'write: {file_diemthi_path}')
+        except Exception as e:
+            logger.error(e)
     logger.info('done')
